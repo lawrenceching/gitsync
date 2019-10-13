ff6571e51bdb0e50272a59ce7f5595e2390012ff
Lawrence Ching <lawrence.ching.zh@gmail.com>
2019-10-13 16:15:56 +0800

    Fixed the issue that GitSync is uanble to pull latest commit if target folder already exist

--- wikijs/sync.sh
+++ wikijs/sync.sh
@@ -1,4 +1,10 @@
-git clone git@github.com:Requarks/wiki.git
-cd wiki
+if [ -d "wiki" ]; then
+  cd wiki
+  git pull
+else
+  git clone git@github.com:Requarks/wiki.git
+  cd wiki
+fi
+
 git remote add gitee git@gitee.com:gitsync/wiki.git
 git push gitee master
