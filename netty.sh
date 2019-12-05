050e0f7d4acecbfecae979c31b4f3dc4f64a072e
Lawrence Ching <lawrence.ching.zh@gmail.com>
2019-12-06 00:14:02 +0800

    Fixed the initial synchronization

--- netty.sh
+++ netty.sh
@@ -17,5 +17,12 @@ git remote add github $GITHUB
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+if [ $? -ne 0 ]; then
+  echo "It looks like an empty project. It generally appears at first synchronization. Trying to pulling from $GITHUB"
+  git pull github
+fi
+
+echo ">>> $BRANCH"
+
 git pull github $BRANCH
 git push -u gitee $BRANCH
