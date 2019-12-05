290506af502ab506d535e387bf529ee4610794dd
Lawrence Ching <lawrence.ching.zh@gmail.com>
2019-12-05 22:43:26 +0800

    Support wiki.js

---
+++ wiki.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -x
+
+if [ ! -d "jetty.project" ]; then
+  git clone git@gitee.com:gitsync/wiki.git
+fi
+
+cd jetty.project
+
+git remote add gitee git@gitee.com:gitsync/wiki.git
+git remote add github git@github.com:Requarks/wiki.git
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+git pull github $BRANCH
+git push -u gitee $BRANCH
