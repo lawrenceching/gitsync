c70b776fb085560d0ed460c14700c2b66d14e263
Lawrence Ching <lawrence.ching.zh@gmail.com>
2019-12-06 21:16:10 +0800

    Pul all branches from GitHub

--- gitsync.sh
+++ gitsync.sh
@@ -32,6 +32,7 @@ function sync {
     git pull github $BRANCH
     git push -u gitee $BRANCH
 
+    git pull github
     git branch -a
 
     git --no-pager branch -a | grep github | sed 's/*//g' | sed 's/ //g' | sed 's/remotes\/github\///g'
