fb1e6f8def351f016a5d0e694192437bd4508d8b
Lawrence Ching <lawrence.ching.zh@gmail.com>
2019-12-06 08:56:05 +0800

    Update netty.sh and wiki.sh to reuse gitsync.sh

--- netty.sh
+++ netty.sh
@@ -1,31 +1,3 @@
 #!/usr/bin/env bash
-set -x
-
-GITHUB="git@github.com:netty/netty.git"
-GITEE="git@gitee.com:gitsync/netty.git"
-
-DIR=$(echo $GITHUB | cut -d '/' -f 2 | cut -d '.' -f 1)
-
-if [ ! -d "$DIR" ]; then
-  git clone $GITEE
-fi
-
-cd $DIR
-
-git remote add gitee $GITEE
-git remote add github $GITHUB
-
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-if [ $? -ne 0 ]; then
-  echo "It looks like an empty project. It generally appears at first synchronization. Trying to pulling from $GITHUB"
-  git pull github
-  BRANCH=$(git rev-parse --abbrev-ref HEAD)
-fi
-
-echo ">>> $BRANCH"
-
-git pull github $BRANCH
-git push -u gitee $BRANCH
-
-git --no-pager branch -a
+source ./gitsync.sh
+sync "git@github.com:netty/netty.git" "git@gitee.com:gitsync/netty.git"
--- wiki.sh
+++ wiki.sh
@@ -1,16 +1,2 @@
-#!/usr/bin/env bash
-set -x
-
-if [ ! -d "wiki" ]; then
-  git clone git@gitee.com:gitsync/wiki.git
-fi
-
-cd wiki
-
-git remote add gitee git@gitee.com:gitsync/wiki.git
-git remote add github git@github.com:Requarks/wiki.git
-
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-git pull github $BRANCH
-git push -u gitee $BRANCH
+source ./gitsync.sh
+sync "git@github.com:Requarks/wiki.git" "git@gitee.com:gitsync/wiki.git"
