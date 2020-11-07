f42dbd7facb312140872272c26cca30f792941c3
Lawrence Ching <lawrenceching@users.noreply.github.com>
2020-11-07 16:13:29 +0800

    Update jetty.project.sh

--- jetty.project.sh
+++ jetty.project.sh
@@ -2,4 +2,4 @@
 set -x
 
 source ./gitsync.sh
-sync "git@github.com:eclipse/jetty.project.git" "git@gitee.com:gitsync/jetty.project.git"
+sync "https://github.com/eclipse/jetty.project.git" "git@gitee.com:gitsync/jetty.project.git"
